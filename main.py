857aa1c640cb2656863869c4f3897b304adbd26e
Martin Bruse <zondolfin@gmail.com>
2021-11-02 16:05:44 +0100

    Changed the ports to not collide with other services.

--- main.py
+++ main.py
@@ -39,5 +39,5 @@ def startup():
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000, log_level="info")
+    uvicorn.run(app, host="0.0.0.0", port=8032, log_level="info")
 
